580a4148c0409016606b580efdd86b85ff705785
Arina <mitreanuarina@gmail.com>
2024-10-04 16:16:30 +0300

    first commit

---
+++ src/edu/tekwill/academy/computer/Computer.java
@@ -0,0 +1,16 @@
+package edu.tekwill.academy.computer;
+
+import edu.tekwill.academy.computer.components.Processor;
+import edu.tekwill.academy.computer.components.Motherboard;
+import edu.tekwill.academy.computer.components.HardDrive;
+import edu.tekwill.academy.computer.peripherals.Monitor;
+import edu.tekwill.academy.computer.peripherals.Mouse;
+import edu.tekwill.academy.computer.peripherals.Keyboard;
+public class Computer {
+    Processor processor;
+    Motherboard motherboard;
+    HardDrive hardDrive;
+    Monitor monitor;
+    Mouse mouse;
+    Keyboard keyboard;
+}
---
+++ src/edu/tekwill/academy/computer/components/Motherboard.java
@@ -0,0 +1,5 @@
+package edu.tekwill.academy.computer.components;
+
+public class Motherboard {
+
+}
